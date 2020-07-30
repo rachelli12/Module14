2eda9234afeeab50e18292244021d008b2ff8d4d
rachelli <brli@dmacc.edu>
2020-07-29 22:43:23 -0500

    initial commit. not tested

---
+++ anagram_class.py
@@ -0,0 +1,36 @@
+"""
+Author: Rachel Li
+Program: anagram_class.py
+Last date modified: 29/07/2020
+
+The purpose of this program is to write class with exception for anagram
+"""
+class InvalidWordException(Exception):
+    ''' invalid word exception '''
+    pass
+
+class Anagram():
+    """Anagram class"""
+    def __init__(self, word):
+        word_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ")
+        if not (word_characters.issuperset(word)):
+            raise InvalidWordException
+        self.word = word
+        self.sorted_anagram = self.arrange_letter()
+
+    def arrange_letter(self):
+        """
+        :return: sorted result of letters
+        """
+        lower_case = self.word.strip().lower()
+        alphabet = sorted(list(lower_case))
+        return ''.join(sorted(alphabet))
+
+    def sort_anagram(self):
+        return self.sorted_anagram
+
+    def __str__(self):
+        return f'Word: {self.word}, Anagram: {self.sorted_anagram}'
+
+    def __repr__(self):
+        return "Anagram: " + self.__str__()
