587937d2d6f52ea205fb77c26ebcb96855e1d81f
rachelli <brli@dmacc.edu>
2020-07-31 16:10:18 -0500

    don't know how to display output

--- anagram/anagram_gui.py
+++ anagram/anagram_gui.py
@@ -1,20 +1,33 @@
+"""
+Program: anagram_gui.py
+Author: Rachel Li
+Last date modified: 31/07/2020
+
+The purpose of this program is to create GUI with Database
+and produce GUI of anagram that displays output of anagram.
+"""
 import sys
-from anagram import anagram_functions as af
+from anagram.anagram_class import *
+from anagram.anagram_functions import *
 from anagram.anagram_database import *
 import tkinter as tk
 from tkinter import messagebox
 
-class GUI(tk.Tk):
+class entry_GUI(tk.Tk):
     DATABASE = 'anagram_database.db'
+
     def __init__(self):
         tk.Tk.__init__(self)
         self.title("Anagram Generator")
 
-        self.start_label = tk.Label(self, text='Enter your word here', width=25).grid(row=2, column=0)
-        self.start_enter = tk.Entry(self).grid(row=2, column=1)
-        self.enter_button = tk.Button(self, text='Enter', command=self.create_database).grid(row=3)
+        self.start_label = tk.Label(self, text='Enter your word here', width=25).grid(row=1, column=0)
+        self.start_enter = tk.Entry(self).grid(row=1, column=1)
+        self.enter_button = tk.Button(self, text='Enter', command=self.create_database, width= 25).grid(row=2)
 
-        self.exit_button = tk.Button(self, text='Exit', width=25, command=self.destroy).grid(row=5)
+        self.display_result_label = tk.Label(self, text='Results').grid(row=3, column=0)
+        self.display_result_message = messagebox.showinfo(command=result)
+
+        self.exit_button = tk.Button(self, text='Exit', width=25, command=self.destroy).grid(row=4)
 
     def create_database(self):
         create_tables(self.DATABASE)
@@ -31,6 +44,9 @@ class GUI(tk.Tk):
         for anagram in anagrams:
             print(anagram)
 
+    def result(self):
+
+
+
 if __name__ == '__main__':
-    gui = GUI()
-    gui.mainloop()
+    entry_GUI().mainloop()
